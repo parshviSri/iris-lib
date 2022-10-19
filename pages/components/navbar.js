84a59711c851c84816a77e5bdc322cffc6ebb21f
parshviSri <parshvisri@gmail.com>
2022-10-19 10:20:34 +0530

    navbar of the app
    Signed-off-by: parshviSri <parshvisri@gmail.com>

---
+++ pages/components/navbar.js
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+const NavBar = () => {
+  
+  return (
+    <>
+      <ConnectButton />
+    </>
+  );
+  
+    
+
+};
+export default NavBar;
